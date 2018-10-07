487c5bf317d27f367181363a024e866c0613e046
yuan <2570837397@qq.com>
2018-10-07 15:58:29 +0800

    修改了SpringcloudZuul类中的main方法

--- spring-cloud-zuul/src/main/java/com/yuan/springcloud/SpringCloudZuul.java
+++ spring-cloud-zuul/src/main/java/com/yuan/springcloud/SpringCloudZuul.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy //开启zuul路由网关的功能(作用：请求的过滤和代理)
 public class SpringCloudZuul {
 //throws Exception
-    public static void main(String args[]){
+    public static void main(String args[])throws Exception{
         SpringApplication.run(SpringCloudZuul.class,args);
 //        System.out.print("");
     }
